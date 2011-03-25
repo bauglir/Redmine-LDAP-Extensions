0335eb64dfaacfb6056e41ec474c04dc4ba945a0
Joris Kraak <joris@majorfail.com>
2011-03-25 22:07:42 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_ldap_extensions do
+  name 'LDAP Extensions'
+  author 'Joris Kraak'
+  description 'A plugin which adds some additional LDAP functionality to Redmine/ChiliProject'
+  version '0.0.1'
+  url 'http://projects.majorfail.com/projects/ldap_extensions/'
+  author_url 'http://majorfail.com/'
+end
